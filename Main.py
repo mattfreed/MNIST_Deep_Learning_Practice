6926a1251167805a8ab968e06f768c2a8c3a4b8e
mattfreed <mwfreed@wpi.edu>
2019-07-03 15:25:07 -0400

    implementing models

--- Main.py
+++ Main.py
@@ -27,4 +27,37 @@ for i in range(cols):
     for j in range(num_classes):
         x_selected = X_train[y_train == j]
         axs[j][i].imshow(x_selected[random.randint(0,len(x_selected-1)),:, :], cmap = plt.get_cmap("gray"))
+        axs[j][i].axis("off")
+        if i == 2:
+            axs[j][i].set_title(str(j))
+            num_of_samples.append(len(x_selected))
+
 plt.show()
+print(num_of_samples)
+plt.figure(figsize=(12,4))
+plt.bar(range(0,num_classes),num_of_samples)
+plt.show()
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test,10)
+
+X_train = X_train/255
+X_test = X_test/255
+
+num_pixels = 784
+X_train = X_train.reshape(X_train.shape[0], num_pixels)
+X_test = X_test.reshape(X_test.shape[0], num_pixels)
+
+def create_model():
+    model = Sequential()
+    model.add(Dense(10, input_dim=num_pixels, activation="relu"))
+    model.add(Dense(10, activation="relu"))
+    model.add(Dense(num_classes, activation="softmax"))
+    model.compile(Adam(lr = 0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+model = create_model()
+print(model.summary())
+
+model.fit(X_train, y_train, validation_split=0.1, epochs=10, batch_size= 200, verbose = 1, shuffle = 1)
+
