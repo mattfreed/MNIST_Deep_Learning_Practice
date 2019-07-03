ec1686a6207044c34ad8762323dcd932c09409cb
mattfreed <mwfreed@wpi.edu>
2019-07-03 16:58:29 -0400

    Add guessing image

--- Main.py
+++ Main.py
@@ -7,6 +7,10 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 import random
+import requests
+from PIL import Image
+import cv2
+
 
 np.random.seed(0)
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -51,6 +55,7 @@ X_test = X_test.reshape(X_test.shape[0], num_pixels)
 def create_model():
     model = Sequential()
     model.add(Dense(10, input_dim=num_pixels, activation="relu"))
+    model.add(Dense(30, activation="relu"))
     model.add(Dense(10, activation="relu"))
     model.add(Dense(num_classes, activation="softmax"))
     model.compile(Adam(lr = 0.01), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -61,3 +66,29 @@ print(model.summary())
 
 model.fit(X_train, y_train, validation_split=0.1, epochs=10, batch_size= 200, verbose = 1, shuffle = 1)
 
+score = model.evaluate(X_test, y_test, verbose = 0)
+print('Test Score:',score[0])
+
+print('Test acc:',score[1])
+
+
+url = 'https://www.researchgate.net/profile/Jose_Sempere/publication/221258631/figure/fig1/AS:305526891139075@1449854695342/Handwritten-digit-2.png'
+response = requests.get(url, stream = True)
+img = Image.open(response.raw)
+plt.imshow(img)
+plt.show()
+
+
+img_array = np.asarray(img)
+resized = cv2.resize(img_array, (28,28))
+gray_scale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+image = cv2.bitwise_not(gray_scale)
+plt.imshow(image,cmap = plt.get_cmap("gray"))
+print(image.shape)
+plt.show()
+
+image = image/255
+image = image.reshape(1,784)
+print(image)
+prediction = model.predict_classes(image)
+print("prediction: ", str(prediction))
